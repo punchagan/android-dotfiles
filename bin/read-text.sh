de8211be08374e79ad291a677ed77cfdd3ab248f
Puneeth Chaganti <punchagan@muse-amuse.in>
2022-11-12 00:11:01 +0530

    read-text: Extract UI for firing tts-speak to separate script

---
+++ bin/read-text.sh
@@ -0,0 +1,17 @@
+#!/data/data/com.termux/files/usr/bin/env bash
+
+LANGS="en,hi,te,kn"
+RATES="1,1.25,1.5,1.75,2,2.25,2.5"
+
+function sigexit_capture() {
+    # NOTE: Termux API doesn't interrup in-progress termux-tts-speak. So, we kill it ourselves when the script is interrupted.
+    echo "Killing android TTS service."
+    am stopservice 'com.termux.api/.TextToSpeechAPI$TextToSpeechService' > /dev/null
+}
+
+trap sigexit_capture EXIT
+
+lang=$(termux-dialog radio -t "Language" -v "${LANGS}" | jq -r .text)
+rate=$(termux-dialog radio -t "Speaking Rate" -v "${RATES}" | jq -r .text)
+
+termux-tts-speak -n IN -l "${lang:-en}" -r "${rate:-1}" $@
